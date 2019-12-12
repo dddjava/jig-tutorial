4a0dd1192fd2b1adbef39c410bf71c868d4dcb94
irof <irof.ocean@gmail.com>
2019-12-12 18:00:46 +0900

    パッケージの関連で開発する/orderとshipmentを追加

---
+++ src/main/java/jig/tutorial/domain/model/order/Order.java
@@ -0,0 +1,10 @@
+package jig.tutorial.domain.model.order;
+
+import jig.tutorial.domain.model.product.Product;
+
+/**
+ * 注文
+ */
+public class Order {
+    Product product;
+}
---
+++ src/main/java/jig/tutorial/domain/model/shipment/Shipment.java
@@ -0,0 +1,12 @@
+package jig.tutorial.domain.model.shipment;
+
+import jig.tutorial.domain.model.order.Order;
+import jig.tutorial.domain.model.product.Product;
+
+/**
+ * 出荷
+ */
+public class Shipment {
+    Product product;
+    Order order;
+}
